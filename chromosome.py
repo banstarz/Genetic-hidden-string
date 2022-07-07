6eb3d13d87cae3fe231410bbb736da69fecdcbf3
banstarz <renatk730@gmail.com>
2022-07-07 14:38:24 +0700

    Chosen more appropriate names for variables

--- chromosome.py
+++ chromosome.py
@@ -1,37 +1,41 @@
 import random
 
 class Chromosome():
-    def __init__(self, string = None, original = 'привет мир!', threshold = 0.9):
-        self.string = string
+
+    def __init__(self, 
+                genes = None, 
+                original = 'привет мир!', 
+                mutation_probability = 0.1):
+        self.genes = genes
         self.original = original
         self.fitness = None
         self.chars = ' .?!абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
-        self.threshold = threshold
+        self.mutation_probability = mutation_probability
     
-    def RandomInit(self):
-        self.string = random.choices(self.chars, k = len(self.original))
-        self.string = ''.join(self.string)
+    def set_random_string(self):
+        self.genes = random.choices(self.chars, k = len(self.original))
+        self.genes = ''.join(self.genes)
  
-    def FitnessFunction(self):
+    def calculate_fitness_function(self):
         if self.fitness == None:
             counter = 0
             for i in range(len(self.original)):
-                if self.string[i] == self.original[i]:
+                if self.genes[i] == self.original[i]:
                     counter+=1
             self.fitness = counter/len(self.original)
         return self.fitness
 
-    def Crossing(self, other):
+    def cross_with(self, other):
         vec = [random.choice([0, 1]) for i in range(len(self.original))]
-        ParentString = [self.string, other.string]
+        ParentString = [self.genes, other.genes]
         string1 = ''.join([ParentString[vec[i]][i] for i in range(len(self.original))])
         string2 = ''.join([ParentString[not vec[i]][i] for i in range(len(self.original))])
-        child1 = Chromosome(string1, self.original, self.threshold)
-        child2 = Chromosome(string2, self.original, self.threshold)
+        child1 = Chromosome(string1, self.original, self.mutation_probability)
+        child2 = Chromosome(string2, self.original, self.mutation_probability)
         return (child1, child2)
-    
-    def Changeover(self):
+
+    def mutate(self):
         for i in range(len(self.original)):
-            if random.random() > self.threshold:
+            if random.random() < self.mutation_probability:
                 char = random.choice(self.chars)
-                self.string = self.string[0:i] + char + self.string[i+1:]
+                self.genes = self.genes[0:i] + char + self.genes[i+1:]
--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -2,75 +2,77 @@ import random
 from chromosome import Chromosome
 
 class GeneticAlgorithm():
-    def __init__(self, PopulationSize = 200, threshold = 0.9, 
-        GenBound = 200, Elite = 15, original = 'привет мир!'):
-        self.PopulationSize = PopulationSize
-        self.threshold = threshold
-        self.GenBound = GenBound
+
+    def __init__(self, 
+                population_size = 200, 
+                mutation_probability = 0.1, 
+                max_generation = 200, 
+                Elite = 15, 
+                original = 'привет мир!'):
+        self.population_size = population_size
+        self.mutation_probability = mutation_probability
+        self.max_generation = max_generation
         self.original = original                
-        self.Population = []
+        self.population = []
         self.Elite = Elite
-        self.ChildList = [0 for i in range(2*self.PopulationSize)]
-        self.GenNum = 0
-        self.Weights = []
+        self.ChildList = [0 for i in range(2*self.population_size)]
+        self.weights = []
         
-        
-    def Process(self):
-        if not self.Population:
-            self.StartPopulationInit()
-        self.EvalFF(self.Population)
-        for i in range(self.GenBound):
-            self.EvalWeights()
+    def run(self):
+        self.initialize_population()
+        self.evaluate_fitness_for_each_chromosome_in(self.population)
+        for i in range(self.max_generation):
+            self.evaluate_selection_weights()
             self.CrossOver()
             self.Mutation()
             self.NextGen()
-            self.GenNum += 1
-            if self.Population[0].string == self.original:
-                print(self.Population[0].string)
+            if self.population[0].genes == self.original:
+                print(self.population[0].genes)
                 break
-            if i%100 == 0:
-                print(self.Population[0].string)
+            if i % 100 == 0:
+                print(self.population[0].genes)
                 
-    def StartPopulationInit(self):
-        self.Population = [Chromosome(original = self.original, threshold = self.threshold)
-                           for i in range(self.PopulationSize)]
-        for i in range(self.PopulationSize):
-            self.Population[i].RandomInit()
+    def initialize_population(self):
+        self.population = [Chromosome(original = self.original, 
+                                        mutation_probability = self.mutation_probability)
+                           for i in range(self.population_size)]
+        for i in range(self.population_size):
+            self.population[i].set_random_string()
     
-    def EvalFF(self, Population):
-        for i in range(len(Population)):
-            Population[i].FitnessFunction()
+    def evaluate_fitness_for_each_chromosome_in(self, population):
+        for i in range(len(population)):
+            population[i].calculate_fitness_function()
     
-    def EvalWeights(self):
-        PopSumFF = 0
-        self.Weights = []
-        PopSumFF = sum([self.Population[i].FitnessFunction() for i in range(self.PopulationSize)])
-        for i in range(self.PopulationSize):
-            self.Weights.append(self.Population[i].FitnessFunction()/PopSumFF)
-            
-    def Selection(self):
-        parent1, parent2 = random.choices(self.Population, weights = self.Weights, k=2)
-        return (parent1, parent2)
+    def evaluate_selection_weights(self):
+        total_population_fitness = 0
+        self.weights = []
+        total_population_fitness = sum([chromosome.calculate_fitness_function() for chromosome in self.population])
+        for chromosome in self.population:
+            self.weights.append(chromosome.calculate_fitness_function()/total_population_fitness)
 
     def CrossOver(self):
-        for i in range(self.PopulationSize):
+        for i in range(self.population_size):
             parent1, parent2 = self.Selection()
-            child1, child2 = parent1.Crossing(parent2)
+            child1, child2 = parent1.cross_with(parent2)
             self.ChildList[i] = child1
-            self.ChildList[i + self.PopulationSize] = child2
+            self.ChildList[i + self.population_size] = child2
+            
+    def Selection(self):
+        parent1, parent2 = random.choices(self.population, weights = self.weights, k=2)
+        return (parent1, parent2)
             
     def Mutation(self):
         for i in range(len(self.ChildList)):
-            self.ChildList[i].Changeover()
+            self.ChildList[i].mutate()
     
     def NextGen(self):
-        sorted(self.Population, key = lambda x: x.FitnessFunction(), reverse = True)
-        sorted(self.ChildList, key = lambda x: x.FitnessFunction(), reverse = True)
+        sorted(self.population, key = lambda x: x.calculate_fitness_function(), reverse = True)
+        sorted(self.ChildList, key = lambda x: x.calculate_fitness_function(), reverse = True)
         j=0
         for i in range(self.Elite):
-            if self.Population[i].FitnessFunction() <= self.ChildList[j].FitnessFunction():
-                self.Population[i] = self.ChildList[j]
+            if self.population[i].calculate_fitness_function() <= self.ChildList[j].calculate_fitness_function():
+                self.population[i] = self.ChildList[j]
                 j+=1
-        for i in range(self.Elite, self.PopulationSize):
-            self.Population[i] = self.ChildList[j]
+        for i in range(self.Elite, self.population_size):
+            self.population[i] = self.ChildList[j]
             j+=1
